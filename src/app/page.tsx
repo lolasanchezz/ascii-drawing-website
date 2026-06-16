8990411ba9c1cf4ab12fe47c50b4882a18672501
lolasanchezz <lolansanchez@icloud.com>
2026-06-16 18:47:15 -0400

    first updating done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-'use client'
+"use client";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
 export default function Home() {
-  
   const width = 500;
   const height = 500;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Easel width={width} height={height} ></Easel>
+        <Easel width={width} height={height}></Easel>
       </main>
     </div>
   );
@@ -17,27 +16,56 @@ export default function Home() {
 const Easel = (props: { width: number; height: number }) => {
   const fontHeight = 12;
   const fontWidth = 10;
-  const [finalRender, setFinalRender] = useState([] as string[][])
- 
+
+  const [finalRender, setFinalRender] = useState([] as string[][]);
+
   useEffect(() => {
-    let tmpArray = []
-    for (let i = 0; i < (props.height/fontHeight); i++) {
+    let tmpArray = [];
+    for (let i = 0; i < props.height / fontHeight; i++) {
       tmpArray[i] = [];
-      for (let j = 0; j < (props.width/fontWidth); j++) {
+      for (let j = 0; j < props.width / fontWidth; j++) {
         tmpArray[i].push(" ");
       }
     }
-    setFinalRender(tmpArray)
+    setFinalRender(tmpArray);
   }, [props.width, props.height]);
-  console.log(finalRender)
+  //unbelievable
+  function updateRender(width: number, height: number, newChar: string) {
+    const newArr = finalRender.map((arr, i) => {
+      if (i === height) {
+        return arr.map((char, i2) => {
+          if (i2 === width) {
+            console.log("updated");
+            return newChar;
+          } else {
+            return char;
+          }
+        });
+      } else {
+        return arr;
+      }
+    });
+    console.log(newArr)
+    setFinalRender(newArr);
+  }
+
   return (
-    <div className={styles.easelDiv} style={{width: props.width, height: props.height}}>
+    <div
+      className={styles.easelDiv}
+      style={{ width: props.width, height: props.height }}
+      onClick={(click) => {
+        click.preventDefault();
+        
+        updateRender(
+          Math.trunc(click.clientX / fontWidth),
+          Math.trunc(click.clientY / fontHeight),
+          "#",
+        );
+      }}
+    >
       {finalRender.map((row, i) => {
-        console.log(row)
-        return (
-          <p key = {i}>{row.join("")}</p>
-        )
+        return <p key={i}>{row.join("")}</p>;
       })}
     </div>
-  )
+  );
 };
