a43ee07a1d75331b10d9183b80421ecd0647815b
lolasanchezz <lolansanchez@icloud.com>
2026-06-17 00:34:26 -0400

    ratios figured out i think

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,17 +14,21 @@ export default function Home() {
 }
 
 const Easel = (props: { width: number; height: number }) => {
-  const fontHeight = 10;
-  const fontWidth = 5;
+  const styleFontHeight = 12;
+  const fontHeight = styleFontHeight * 1.2;
+ const fontWidth = styleFontHeight * 0.56;
+  
   const [mouseDown, setMouseDown] = useState(false);
   const [finalRender, setFinalRender] = useState([] as string[][]);
-
+  
   useEffect(() => {
     let tmpArray = [];
     for (let i = 0; i < props.height / fontHeight; i++) {
       tmpArray[i] = [];
       for (let j = 0; j < props.width / fontWidth; j++) {
-        tmpArray[i].push(" ");
+        //tmpArray[i].push("#");
+        tmpArray[i].push("⠀");
+       
       }
     }
     setFinalRender(tmpArray);
@@ -52,7 +56,7 @@ const Easel = (props: { width: number; height: number }) => {
   return (
     <div
       className={styles.easelDiv}
-      style={{ width: props.width, height: props.height }}
+      style={{ width: `${props.width}px`, height: `${props.height}px`, fontSize: styleFontHeight}}
       onMouseDown={(e) => {
         e.preventDefault()
         setMouseDown(true);
@@ -65,6 +69,7 @@ const Easel = (props: { width: number; height: number }) => {
             {row.map((char, j) => {
               return (
                 <span
+                className = {styles.char}
                   key={j}
                   onMouseOver={(client) => {
                     if (mouseDown) {
